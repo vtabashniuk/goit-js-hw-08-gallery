1a7ff1c0b7ea1234713a7b7a16bf6d0e99272d91
Volodymyr Tabashniuk <vtabashniuk@outlook.com>
2021-07-06 01:27:32 +0300

    Добавляет открытие модального окна с оригинальной картинкой

--- js/task-hw-08.js
+++ js/task-hw-08.js
@@ -1,14 +1,18 @@
 import galleryItems from './app.js';
 
 const galleryRef = document.querySelector('.js-gallery');
+const openModalRef = document.querySelector('.lightbox');
+const closeModalRef = document.querySelector('[data-action=close-lightbox]');
+const imageAtModal = document.querySelector('.lightbox__image');
 
+// функция рендеринга галереи.
 function galleryItemsRender(images) {
   return images
-    .map(({ preview, description }) => {
+    .map(({ preview, original, description }) => {
       return `
     <li class="gallery__item">
-        <a class="gallery__link">
-            <img class="gallery__image" src="${preview}" alt="${description}" />
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
         </a>
     </li>
     `;
@@ -16,4 +20,23 @@ function galleryItemsRender(images) {
     .join(' ');
 }
 
+//вставляеи код галереи в разметку
 galleryRef.insertAdjacentHTML('afterbegin', galleryItemsRender(galleryItems));
+
+galleryRef.addEventListener('click', event => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  openModalRef.classList.add('is-open');
+  imageAtModal.src = event.target.dataset.source;
+  imageAtModal.alt = event.target.alt;
+});
+
+closeModalRef.addEventListener('click', event => {
+  openModalRef.classList.remove('is-open');
+  imageAtModal.src = '';
+  imageAtModal.alt = '';
+});
