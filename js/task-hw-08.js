4400feae7d3ddb7d42d035e49307193f02058986
Volodymyr Tabashniuk <vtabashniuk@outlook.com>
2021-07-06 09:04:48 +0300

    Добавляет закрытие по нажатии кнопки Esc или клик в backdpor

--- js/task-hw-08.js
+++ js/task-hw-08.js
@@ -1,9 +1,13 @@
 import galleryItems from './app.js';
 
 const galleryRef = document.querySelector('.js-gallery');
-const openModalRef = document.querySelector('.lightbox');
-const closeModalRef = document.querySelector('[data-action=close-lightbox]');
+const openModalRef = document.querySelector('.js-lightbox');
 const imageAtModal = document.querySelector('.lightbox__image');
+
+//вставляеи код галереи в разметку
+galleryRef.insertAdjacentHTML('afterbegin', galleryItemsRender(galleryItems));
+
+galleryRef.addEventListener('click', onClickOpenModal);
 
 // функция рендеринга галереи.
 function galleryItemsRender(images) {
@@ -19,11 +23,8 @@ function galleryItemsRender(images) {
     })
     .join(' ');
 }
-
-//вставляеи код галереи в разметку
-galleryRef.insertAdjacentHTML('afterbegin', galleryItemsRender(galleryItems));
-
-galleryRef.addEventListener('click', event => {
+// функция открытия модального окна по клику на картинке
+function onClickOpenModal(event) {
   event.preventDefault();
 
   if (event.target.nodeName !== 'IMG') {
@@ -33,10 +34,28 @@ galleryRef.addEventListener('click', event => {
   openModalRef.classList.add('is-open');
   imageAtModal.src = event.target.dataset.source;
   imageAtModal.alt = event.target.alt;
-});
-
-closeModalRef.addEventListener('click', event => {
+  openModalRef.addEventListener('click', onClickCloseModal);
+  window.addEventListener('keydown', onKeyPressCloseModal);
+}
+// функция закрытия модального окна по клику мышкой
+function onClickCloseModal(event) {
+  if (
+    event.target.classList.contains('lightbox__overlay') ||
+    event.target.classList.contains('lightbox__button')
+  ) {
+    closeModal();
+  }
+}
+// функция закрытия модального окна
+function closeModal() {
   openModalRef.classList.remove('is-open');
+  window.removeEventListener('keydown', onKeyPressCloseModal);
   imageAtModal.src = '';
   imageAtModal.alt = '';
-});
+}
+// функция закрытия модального окна по нажатии кнопки
+function onKeyPressCloseModal(event) {
+  if (event.code === 'Escape') {
+    closeModal();
+  }
+}
