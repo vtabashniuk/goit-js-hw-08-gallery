61086b574de3f6192466d2e03959fc5cf212aacb
Volodymyr Tabashniuk <vtabashniuk@outlook.com>
2021-07-08 20:25:59 +0300

    Добавляет lazyloading на картинки

--- js/task-hw-08.js
+++ js/task-hw-08.js
@@ -16,7 +16,7 @@ function galleryItemsRender(images) {
       return `
     <li class="gallery__item">
         <a class="gallery__link" href="${original}">
-            <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+            <img class="gallery__image" loading="lazy" src="${preview}" data-source="${original}" alt="${description}" />
         </a>
     </li>
     `;
